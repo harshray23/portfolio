6d01f6e54e8e01eb14344d22ee5dcee621020cb3
laptop <laptopharsh0108@gmail.com>
2025-07-10 16:33:20 +0000

    remove this line from projects You can find more on my GitHub

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,7 +12,7 @@ export default function ProjectsPage() {
           <div className="container">
             <h2 className="text-3xl font-headline font-bold text-center mb-4">All Projects</h2>
             <p className="max-w-3xl mx-auto text-center text-lg text-muted-foreground mb-12">
-              Here are some of the projects I've worked on. You can find more on my GitHub.
+              Here are some of the projects I've worked on.
             </p>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
               {projects.map((project, index) => (

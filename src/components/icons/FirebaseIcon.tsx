8128a5e2884e5bd52247ec676f1381a703fc106c
laptop <laptopharsh0108@gmail.com>
2025-08-16 19:28:33 +0000

    not profile pic but actually the svg used for skills animation

--- src/components/icons/FirebaseIcon.tsx
+++ src/components/icons/FirebaseIcon.tsx
@@ -3,6 +3,6 @@ import type { SVGProps } from 'react';
 
 export function FirebaseIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M4.32 20.32l.82-13.43L12 2l6.86 4.89l.82 13.43L12 22zm3.33-3.15l-1-6.35l4.3-3.07v12.44zm7.7 0l1-6.35l-4.3-3.07v12.44z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M4.32 20.32l.82-13.43L12 2l6.86 4.89l.82 13.43L12 22zm3.33-3.15l-1-6.35l4.3-3.07v12.44zm7.7 0l1-6.35l-4.3-3.07v12.44z"></path></svg>
   );
 }
--- src/components/icons/PythonIcon.tsx
+++ src/components/icons/PythonIcon.tsx
@@ -3,6 +3,6 @@ import type { SVGProps } from 'react';
 
 export function PythonIcon(props: SVGProps<SVGSVGElement>) {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M14.5 14.25h-5v-4h5V8h-5V4h-.75A4.25 4.25 0 0 0 4.5 8.25v7.5A4.25 4.25 0 0 0 8.75 20h.75v-4h5v-1.75m-6-1.5v-2h2v2zm.75 7.5H8a2.75 2.75 0 0 1-2.75-2.75v-9A2.75 2.75 0 0 1 8 5.25h.75zM9.5 9.75h5v4h-5V16h5v4h.75A4.25 4.25 0 0 0 19.5 15.75v-7.5A4.25 4.25 0 0 0 15.25 4h-.75v4h-5v1.75m6-1.5v-2h-2v2zm-.75-7.5H16a2.75 2.75 0 0 1 2.75 2.75v9A2.75 2.75 0 0 1 16 18.75h-.75z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" {...props}><path fill="currentColor" d="M14.5 14.25h-5v-4h5V8h-5V4h-.75A4.25 4.25 0 0 0 4.5 8.25v7.5A4.25 4.25 0 0 0 8.75 20h.75v-4h5v-1.75m-6-1.5v-2h2v2zm.75 7.5H8a2.75 2.75 0 0 1-2.75-2.75v-9A2.75 2.75 0 0 1 8 5.25h.75zM9.5 9.75h5v4h-5V16h5v4h.75A4.25 4.25 0 0 0 19.5 15.75v-7.5A4.25 4.25 0 0 0 15.25 4h-.75v4h-5v1.75m6-1.5v-2h-2v2zm-.75-7.5H16a2.75 2.75 0 0 1 2.75 2.75v9A2.75 2.75 0 0 1 16 18.75h-.75z"></path></svg>
   );
 }
